9565293d3de43976c1f9ffa3317b7e9f5779bcb7
Joshua Alvarez <Jalvarez-2021317@kinal.edu.gt>
2023-02-07 13:56:32 -0600

    Subiendo Tienda

---
+++ models/categoria.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    proveedor: {
+        type: String,
+        required: [true, 'El proveedor es obligatorio'],
+    },
+    
+    descripcion: {
+        type: String,
+        required: [true, 'La descripcion es obligatorio']
+    },
+    seccion: {
+        type: String,
+        required: true,
+        emun: ['Comestible', 'Ocio','esencial','bebible','literatia']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model('Categoria', CategoriaSchema)
