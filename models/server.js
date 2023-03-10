40b6a13658f2e007a350ebad616daa2f43adfc4e
Joshua Alvarez <jalvarez-2021317@kinal.edu.gt>
2023-03-10 10:32:11 -0600

    agregando carrito

--- models/server.js
+++ models/server.js
@@ -5,62 +5,64 @@ const { dbConection } = require('../database/config');
 
 class Server {
 
-    constructor(){
+    constructor() {
 
         this.app = express();
         this.port = process.env.PORT;
         this.paths = {
             buscar: '/api/buscar',
-           auth: '/api/auth',
+            auth: '/api/auth',
             usuario: '/api/usuarios',
             categoria: '/api/categorias',
             producto: '/api/productos',
-            factura:'/api/facturas'
+            factura: '/api/facturas',
+            carrito: '/api/carrito'
         }
 
- 
+
         this.conectarDB();
 
-     
+
         this.middlewares();
-        
-    
+
+
         this.routes();
 
     }
 
 
-  
-    async conectarDB(){
+
+    async conectarDB() {
         await dbConection();
     }
 
-    
-    middlewares(){
 
-       
-        this.app.use( cors() );
+    middlewares() {
 
-        
-        this.app.use( express.json() );
 
-        
-        this.app.use(  express.static('public') );
+        this.app.use(cors());
+
+
+        this.app.use(express.json());
+
+
+        this.app.use(express.static('public'));
 
     }
 
 
-    routes(){
-        this.app.use( this.paths.auth , require('../routes/auth') );
-        this.app.use( this.paths.categoria, require('../routes/categoria'));
-        this.app.use( this.paths.usuario , require('../routes/usuario') );
-        this.app.use( this.paths.producto , require('../routes/productos') );
-        this.app.use(this.paths.factura , require('../routes/factura'))
-        this.app.use(this.paths.buscar , require('../routes/buscar'))
+    routes() {
+        this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.categoria, require('../routes/categoria'));
+        this.app.use(this.paths.usuario, require('../routes/usuario'));
+        this.app.use(this.paths.producto, require('../routes/productos'));
+        this.app.use(this.paths.factura, require('../routes/factura'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
+        this.app.use(this.paths.carrito, require('../routes/carrito'));
     }
 
 
-    listen(){
+    listen() {
         this.app.listen(this.port, () => {
             console.log(`Servidor corriendo en puerto ${this.port}`)
         })
